1121e0178f4d06a82f525d728551385d16f4e657
4ak1f <130276030+4ak1f@users.noreply.github.com>
2024-10-24 15:06:57 +0530

    Update P4.6.cpp

--- P4.6.cpp
+++ P4.6.cpp
@@ -6,18 +6,19 @@
 int main() {
     int array[ARRAY_SIZE] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
     int squared[ARRAY_SIZE];
+    int i;
 
     #pragma omp parallel for
-    for (int i = 0; i < ARRAY_SIZE; i++) {
+    for (i = 0; i < ARRAY_SIZE; i++) {
         squared[i] = array[i] * array[i];  
     }
     
     printf("Original Array:\n");
-    for (int i = 0; i < ARRAY_SIZE; i++) {
+    for (i = 0; i < ARRAY_SIZE; i++) {
         printf("%d ", array[i]);
     }
     printf("\n\nSquared Array:\n");
-    for (int i = 0; i < ARRAY_SIZE; i++) {
+    for (i = 0; i < ARRAY_SIZE; i++) {
         printf("%d ", squared[i]);
     }
    
