cadfbaab91210766e5c9f404ebe6e7f14fe84a5b
4ak1f <130276030+4ak1f@users.noreply.github.com>
2024-10-24 15:03:54 +0530

    Update P4.1.cpp

--- P4.1.cpp
+++ P4.1.cpp
@@ -3,6 +3,7 @@
 #include <omp.h>
 
 void Fib(int n){
+	int i;
 	if(n<=0){
 		printf("Invalid. Enter a positive number\n");
 		return;
@@ -14,11 +15,11 @@ void Fib(int n){
 }
 	
 	#pragma omp for
-	for(int i=2;i<n;i++){
+	for(i=2;i<n;i++){
 		fib[i]=fib[i-1]+fib[i-2];
 }
 	printf("The first %d fibonacci numbers are: \n",n);
-	for(int i=0;i<n;i++){
+	for(i=0;i<n;i++){
 		printf("%lld ",fib[i]);
 	}
 	printf("\n");
